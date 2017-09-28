29dcb4b68d0d84c88b97dffeee9043e3b0c1a8c6
Brian Duemmer <bduemmer1@gmail.com>
2017-09-28 17:12:45 -0400

    Tweaked database access
    adjusted the format and processing of the resultset returned from the
    queries to be more robust

--- dev/db-entry.php
+++ dev/db-entry.php
@@ -4,25 +4,60 @@
 <meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
 <title>Database Tester</title>
 
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 5px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+
 <!-- JS / JQuery functions -->
 <script type="text/javascript" src="/js/jquery.min.js"></script>
 <script type="text/javascript">
 			$(document).ready(function()
 			{
-				// on clicking the button, POST down the sql to the database
+				// on clicking the button, POST down the sql to the database...
 				$("#eval_sql").click(function()
 				{
 					var sql = $("#sql_entry").val();
 					$.post( "dbPostExec.php",
 							{"sql": sql},
-							function(result)
+							function(result) //... and populate the results into a table
 							{
-								$("#res_p").html(result);
+// 								alert(result);
+								rs = JSON.parse(result);
+								
+								var tblDat = "<tr>";
+								
+								for(var c=0; c<rs.cols.length; c++) 
+									{ tblDat += "<th>" +rs.cols[c]+ "</th>" }
+								
+								tblDat += "</tr>";
+
+								for(var r=0; r<rs.data.length; r++) { 
+									tblDat +="<tr>";
+									
+									for(var i=0; i<rs.cols.length; i++)
+										{ tblDat += "<td>" +rs.data[r][rs.cols[i]]+ "</td>"; }
+									
+									tblDat +="</tr>";
+								}
+
+								$("#sql_results").html(tblDat);
 							});
 				});
 			});
-
-				
 </script>
 
 </head>
--- dev/dbPostExec.php
+++ dev/dbPostExec.php
@@ -1,41 +1,45 @@
 <?php
-	require_once $_SERVER['DOCUMENT_ROOT'] . '/dev/GLOBALS.php';
+require_once $_SERVER ['DOCUMENT_ROOT'] . '/dev/GLOBALS.php';
+
+if ($_SERVER ['REQUEST_METHOD'] == "POST") {
+	// fetch post parameters
+	$dbHost = constant ( 'DB_DYSTRACK_HOST' );
+	$dbUser = constant ( 'DB_DYSTRACK_USER' );
+	$dbPass = constant ( 'DB_DYSTRACK_PASS' );
+	$dbDatabase = constant ( 'DB_DYSTRACK_NAME' );
+	$dbStatement = clean ( $_POST ['sql'] );
 	
-	if ($_SERVER ['REQUEST_METHOD'] == "POST") {
-		// fetch post parameters
-		$dbHost = constant('DB_DYSTRACK_HOST');
-		$dbUser = constant('DB_DYSTRACK_USER');
-		$dbPass = constant('DB_DYSTRACK_PASS');
-		$dbDatabase = constant('DB_DYSTRACK_NAME');
-		$dbStatement = clean ( $_POST ['sql'] );
+	// fetch query
+	$db = mysqli_connect ( $dbHost, $dbUser, $dbPass, $dbDatabase ) or die ( 'failed to connect to database: ' . mysqli_error ( $db ) );
+	mysqli_select_db ( $db, $dbDatabase );
+	$result = mysqli_query ( $db, $dbStatement );
+	
+	//$jsonRes->cols[0] = "";
+	$jsonRes = new \stdClass();
+	$jsonRes->data = [];
+	$jsonRes->cols = [];
+	
+	// convert to json
+	if ($result->num_rows > 0) {
 		
-		// fetch query
-		$db = mysqli_connect ( $dbHost, $dbUser, $dbPass, $dbDatabase ) or die ( 'failed to connect to database: ' . mysqli_error ( $db ) );
-		mysqli_select_db ( $db, $dbDatabase );
-		$result = mysqli_query ( $db, $dbStatement );
-		
-		$jsonRes = Array (
-				"No Resultset" 
-		);
-		
-		// convert to json
-		if ($result->num_rows > 0) {
-			
-			$i = 0;
-			while ( $row = $result->fetch_assoc() ) {
-				$jsonRes[$i] = $row;
-				$i++;
-			}
+		$i = 0;
+		while ( $row = $result->fetch_assoc () ) {
+			$jsonRes->data [$i] = $row;
+			$i ++;
 		}
-		
-		echo json_encode ( $jsonRes );
-	} else {
-		echo "ERRROR! Method was not post!";
 	}
 	
-	
-	function clean($data) {
-		$data = trim ( $data );
-		return $data;
+	// get column names
+	foreach ($result->fetch_fields() as $col) {
+		$jsonRes->cols[] = $col->name;
 	}
+	
+	echo json_encode ( $jsonRes );
+} else {
+	echo "ERRROR! Method was not post!";
+}
+function clean($data) {
+	$data = trim ( $data );
+	return $data;
+}
 ?>
